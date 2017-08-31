bdd1ee773609b202f333f05065e5906827eb5eba
training_B8C.03.01 <training_B8C.03.01@accenture.com>
2017-08-31 12:40:32 +0530

    updated source file

--- src/main/java/hello/helloWorld.java
+++ src/main/java/hello/helloWorld.java
@@ -3,6 +3,7 @@ package hello;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+System.out.println("byeWorld");
 		System.out.println("Added newly");
 		System.out.println("Added newly");
 		System.out.println("Added newly");
